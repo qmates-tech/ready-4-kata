f3ae9d7ee2075e76bf64d3e33992071254d0f82b
Daniele Megna <megna.dany@gmail.com>
2023-11-03 18:40:14 +0100

    Update Java ready-2-kata project
    Trying to make it as similar as possibile to the Kotlin one:
    - update build.gradle to have test task well configured,
      removed useless guava dependency and added assertj (useful for katas)
    - no main and no application gradle plugin
    - single test file `FooTest` with simple junitWorks test
    - inner README file to build and run tests
    - keep same version of gradle and gradlewrapper as Kotlin project
    - removed app root module folder and scaffolding package

---
+++ Java/src/test/java/FooTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class FooTest {
+
+    @Test
+    public void junitWorks() {
+        assertTrue(true);
+    }
+
+}
